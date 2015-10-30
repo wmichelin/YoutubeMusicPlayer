96838487af2f7cdd615e6304cad10ace05ad94e0
Walter Michelin <wmichelin@gmail.com>
2015-10-30 19:19:15 -0400

    querying youtube api for video results

--- js/app.js
+++ js/app.js
@@ -1,40 +1,37 @@
-// 2. This code loads the IFrame Player API code asynchronously.
-var tag = document.createElement('script');
+var youtubeController = function() {
 
-tag.src = "https://www.youtube.com/iframe_api";
-var firstScriptTag = document.getElementsByTagName('script')[0];
-firstScriptTag.parentNode.insertBefore(tag, firstScriptTag);
+  this.initialize = function() {
 
-// 3. This function creates an <iframe> (and YouTube player)
-//    after the API code downloads.
-var player;
-function onYouTubeIframeAPIReady() {
-  player = new YT.Player('player', {
-    height: '390',
-    width: '640',
-    videoId: 'M7lc1UVf-VE',
-    events: {
-      'onReady': onPlayerReady,
-      'onStateChange': onPlayerStateChange
-    }
-  });
-}
+    $('#testButton').on('click', function() {
+      var queryString = $('#testInput').val();
+    
+      executeQuery(queryString).done(function(response){
+        response.items.forEach(function(item){
+          console.log(item);
+        });
+      });
+    });
 
-// 4. The API will call this function when the video player is ready.
-function onPlayerReady(event) {
-  event.target.playVideo();
-}
-
-// 5. The API calls this function when the player's state changes.
-//    The function indicates that when playing a video (state=1),
-//    the player should play for six seconds and then stop.
-var done = false;
-function onPlayerStateChange(event) {
-  if (event.data == YT.PlayerState.PLAYING && !done) {
-    setTimeout(stopVideo, 6000);
-    done = true;
+    $(this).on('testEvent', function() {
+      console.log('custom event worked');
+    });
   }
-}
-function stopVideo() {
-  player.stopVideo();
-}
+
+
+  function executeQuery(query) {
+    return new Promise(function(resolve, reject){
+      var url = 'https://www.googleapis.com/youtube/v3/search?part=snippet&key=AIzaSyBGW3_8YPM9IkIFry4QuisFUgvdK_mgtOA&q=' + query;
+      $.ajax({
+        type: "GET",
+        url: url,
+        success: function(results) {
+          resolve(results);
+        }
+      });
+    });
+  }
+
+  return {
+    initialize: self.initialize
+  }
+}();
